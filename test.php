8417af0fddc90f8ac2bd2f0b0c34d6064074198e
MEAJIN <kimo3o98@gmail.com>
2021-07-05 20:58:50 +0900

    최신 In/UP 날려 먹어서 구버전 In/UP으로 대체..

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <p>Name: <?php echo $_POST["name"]; ?></p>
+    <p>ID: <?php echo $_POST["ID"]; ?></p>
+    <p>Passwd: <?php echo $_POST["Passwd"]; ?></p>
+    <p>mail: <?php echo $_POST["mail"]; ?></p>
+    <p>msg: <?php echo $_POST["msg"]; ?></p>
+  </body>
+</html>
